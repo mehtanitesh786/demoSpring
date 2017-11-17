1014a369aac63f6896970cd423bf7285865ab85f
Nitesh Mehta <mehtanitesh786@gmail.com>
2017-11-17 11:14:30 +0530

    Test

--- src/main/java/com/nbmehta/controller/HelloController.java
+++ src/main/java/com/nbmehta/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
-		model.addAttribute("student", "welcome");
+		model.addAttribute("student", "welcome 123");
 		return "hello";
 
 	}
